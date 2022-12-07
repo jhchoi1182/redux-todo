b6087ad95b0115749d23c07ae55ab06bf0686cfe
jhchoi1182 <jhchoi1182@gmail.com>
2022-12-08 01:17:35 +0900

    로컬 스토리지 적용 완료 #4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Router from './Components/Shared/Router';
 
 const App = () => {
+  // const todo = useSelector((state) => state.Todos)
+
+  // localStorage.getItem(todo)
+
   return <Router />
 };
 
--- src/Redux/Modules/Todos.js
+++ src/Redux/Modules/Todos.js
@@ -1,7 +1,10 @@
+import { getLocal, setLocal } from "../util/Local";
+
 const ADD_TODOS = 'ADD_TODOS';
 const DELETE_TODOS = 'DELETE_TODOS';
 const CHANGE_TODOS = 'CHANGE_TODOS';
 const DETAIL_TODOS = 'DETAIL_TODOS';
+
 
 export const addTodos = (payload) => ({ type: ADD_TODOS, payload });
 export const deleteTodos = (payload) => ({ type: DELETE_TODOS, payload });
@@ -9,7 +12,7 @@ export const changeTodos = (payload) => ({ type: CHANGE_TODOS, payload });
 export const detailTodos = (payload) => ({ type: DETAIL_TODOS, payload });
 
 const initialState = {
-  todos: [],
+  todos: getLocal(),
   detail: {}
 };
 
@@ -23,14 +26,18 @@ const todoList = (state = initialState, action) => {
         title: title,
         contents: contents,
       };
+      setLocal([...state.todos, todo])
       return { todos: [...state.todos, todo] };
 
     case DELETE_TODOS:
       const deletedTodo = state.todos.filter((todos) => todos.id !== action.payload);
+      localStorage.setItem('todo', 'DELETED')
+      setLocal(deletedTodo)
       return { ...state, todos: deletedTodo };
 
     case CHANGE_TODOS:
       const changeTodo = state.todos.map((todos) => (todos.id === action.payload ? { ...todos, state: !todos.state } : todos));
+      setLocal(changeTodo)
       return { ...state, todos: changeTodo };
 
     case DETAIL_TODOS:
